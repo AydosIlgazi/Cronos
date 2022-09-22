89bece67d62fd56e7894a552898c669c9faddd56
Aydos İlgazi <aydos.ilgazi@softtech.com.tr>
2022-09-22 10:52:52 +0300

    örnek table configuration eklendi

---
+++ Cronos.Application/Data/Configurations/BannerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cronos.Application.Data.Configurations
+{
+    public class BannerConfiguration : IEntityTypeConfiguration<BannerEntity>
+    {
+        public void Configure(EntityTypeBuilder<BannerEntity> builder)
+        {
+            //builder.Property(b => b.ImageUrl).IsRequired(false);
+            //builder.Property(b => b.AltText).HasMaxLength(200);
+
+        }
+    }
+}
