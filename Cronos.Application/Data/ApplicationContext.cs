aee87db2745e89470df3a47d3f367c4eb7055933
aydos <aydos@DESKTOP-69DNI12>
2022-09-29 10:12:32 +0300

    merge errors silindi

--- Cronos.Application/Data/ApplicationContext.cs
+++ Cronos.Application/Data/ApplicationContext.cs
@@ -37,14 +37,12 @@ namespace Cronos.Application.Data
         {
             return dbSet
                 .Where(
-                   b => b.IsActive == true 
+                   b => b.IsActive == true
                     && b.StartDate >= DateTime.Now
                     && b.EndDate >= DateTime.Now)
                 .OrderBy(b => b.Order)
-            return dbSet
-                    .Where(b => b.IsActive == true && b.IsDeleted == false)
-                    .OrderBy(b => b.Order)
-                    .AsQueryable();
+                .AsQueryable();
+
         }
         //Adminin, etkinliklerin tüm propertylerini görmesi için.  Gülderen Sungur 29.09.2022
         public static IQueryable<T> ShowAllEntity<T>(this DbSet<T> dbSet) where T : BaseEntity
@@ -73,6 +71,6 @@ namespace Cronos.Application.Data
  
 
 
-    }
+    
 }
 
